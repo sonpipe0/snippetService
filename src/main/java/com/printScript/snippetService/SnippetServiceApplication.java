0cd457811a94929d173bfe83924e81f258ea5a4a
sonpipe0 <pipefc2003@gmail.com>
2024-09-24 23:05:13 -0300

    setup

---
+++ src/main/java/com/printScript/snippetService/SnippetServiceApplication.java
@@ -0,0 +1,13 @@
+package com.printScript.snippetService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnippetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnippetServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/printScript/snippetService/SnippetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.printScript.snippetService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnippetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
