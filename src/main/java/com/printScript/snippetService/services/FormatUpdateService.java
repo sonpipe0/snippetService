a050167a7edf001798649de894ab1dcb27697ca7
sonpipe0 <pipefc2003@gmail.com>
2024-11-09 22:42:01 -0300

    continue iteration upon change of lang

--- src/main/java/com/printScript/snippetService/services/FormatUpdateService.java
+++ src/main/java/com/printScript/snippetService/services/FormatUpdateService.java
@@ -3,6 +3,7 @@ package com.printScript.snippetService.services;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,27 +39,29 @@ public class FormatUpdateService {
 
     public void sendFormatMessages(String userId, String token) {
         List<String> snippets = this.getAllSnippets(userId, token);
-        int count = 0;
+        AtomicInteger count = new AtomicInteger();
         for (String snippet : snippets) {
             snippetRepository.findById(snippet).ifPresent(snippetEntity -> {
                 snippetEntity.setFormatStatus(Snippet.Status.IN_PROGRESS);
                 if (!Objects.equals(snippetEntity.getLanguage(), "printscript")) {
                     snippetEntity.setFormatStatus(Snippet.Status.UNKNOWN);
                     snippetRepository.save(snippetEntity);
-                    throw new RuntimeException("Invalid language");
+                    logger.warning("Skipping snippet with id: " + snippet + " due to invalid language");
+                    return;
                 }
                 snippetRepository.save(snippetEntity);
+
+                ConfigPublishEvent snippetEvent = new ConfigPublishEvent();
+                snippetEvent.setSnippetId(snippet);
+                snippetEvent.setUserId(userId);
+                snippetEvent.setType(ConfigPublishEvent.ConfigType.FORMAT);
+                try {
+                    lintProducer.publishEvent(snippetEvent);
+                    count.getAndIncrement();
+                } catch (Exception e) {
+                    logger.warning("Failed to send format message for snippet with id: " + snippetEvent.getSnippetId());
+                }
             });
-            ConfigPublishEvent snippetEvent = new ConfigPublishEvent();
-            snippetEvent.setSnippetId(snippet);
-            snippetEvent.setUserId(userId);
-            snippetEvent.setType(ConfigPublishEvent.ConfigType.FORMAT);
-            try {
-                lintProducer.publishEvent(snippetEvent);
-                count++;
-            } catch (Exception e) {
-                logger.warning("Failed to send format message for snippet with id: " + snippetEvent.getSnippetId());
-            }
         }
     }
 
