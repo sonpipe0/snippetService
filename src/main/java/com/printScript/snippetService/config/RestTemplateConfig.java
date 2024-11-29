5eed8937356492122f2e3d8a4a42b45088f6d1c2
sonpipe0 <pipefc2003@gmail.com>
2024-11-29 12:15:03 -0300

    test correlation id logging

--- src/main/java/com/printScript/snippetService/config/RestTemplateConfig.java
+++ src/main/java/com/printScript/snippetService/config/RestTemplateConfig.java
@@ -4,14 +4,17 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.printScript.snippetService.interceptors.CorrelationIdInterceptor;
 import com.printScript.snippetService.services.RestTemplateService;
 
 @Configuration
 public class RestTemplateConfig {
 
+    private final CorrelationIdInterceptor correlationIdInterceptor = new CorrelationIdInterceptor();
+
     @Bean
     public RestTemplateService printScriptRestTemplate() {
-        RestTemplateBuilder builder = new RestTemplateBuilder();
+        RestTemplateBuilder builder = new RestTemplateBuilder().additionalInterceptors(correlationIdInterceptor);
         RestTemplateService restTemplateService = new RestTemplateService(builder);
         restTemplateService.printScriptRestTemplate(builder);
         return restTemplateService;
@@ -19,7 +22,7 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplateService permissionsRestTemplate() {
-        RestTemplateBuilder builder = new RestTemplateBuilder();
+        RestTemplateBuilder builder = new RestTemplateBuilder().additionalInterceptors(correlationIdInterceptor);
         RestTemplateService restTemplateService = new RestTemplateService(builder);
         restTemplateService.permissionsRestTemplate(builder);
         return restTemplateService;
@@ -27,7 +30,7 @@ public class RestTemplateConfig {
 
     @Bean
     public RestTemplateService bucketRestTemplate() {
-        RestTemplateBuilder builder = new RestTemplateBuilder();
+        RestTemplateBuilder builder = new RestTemplateBuilder().additionalInterceptors(correlationIdInterceptor);
         RestTemplateService restTemplateService = new RestTemplateService(builder);
         restTemplateService.bucketRestTemplate(builder);
         return restTemplateService;
